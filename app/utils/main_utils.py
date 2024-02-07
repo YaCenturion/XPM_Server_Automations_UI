d6407d6f12484f923b6a95106e1bca7825b4be34
YaCenturion <boris.drozdovsky@gmail.com>
2024-02-07 21:29:49 +0200

    DEV: clear code

--- app/utils/main_utils.py
+++ app/utils/main_utils.py
@@ -4,25 +4,6 @@ import paramiko
 import socket
 from datetime import datetime
 from ..models import *
-# from db_preset import create_db_and_update_data  # All for a clear start. Need to kill previous DB.
-
-
-def get_like(way, search_query):
-    query = f"%{search_query}%"
-    if way == 'companies':
-        res = Companies.query.filter(
-                (Companies.name.ilike(query)) |
-                (Companies.full_name.ilike(query)) |
-                (Companies.id.ilike(query)) |
-                (Companies.comments.ilike(query))
-                ).all()
-        print(res)
-    else:
-        res = False
-
-    if not res:
-        res = 0
-    return res
 
 
 def save_in_db(item):
@@ -43,38 +24,6 @@ def get_endpoint(text):
 def show_post_data(form_data):
     for key, value in form_data:
         print(f"{key}: {value}")
-
-
-def company_exists(name, full_name):
-    return Companies.query.filter_by(name=name, full_name=full_name).first() is not None
-
-
-def contract_exists(num_index):
-    return Contracts.query.filter_by(num_index=num_index).first() is not None
-
-
-def domain_exists(name):
-    return Contracts.query.filter_by(name=name).first() is not None
-
-
-def person_exists(name):
-    return Persons.query.filter_by(name=name).first() is not None
-
-
-def get_all_companies():
-    return [(company.id, company.name) for company in Companies.query.all()]
-
-
-def get_all_contracts():
-    return [(contract.id, str(contract.id)) for contract in Contracts.query.all()]
-
-
-def get_all_hostings():
-    return [(hosting.id, str(hosting.id)) for hosting in Hostings.query.all()]
-
-
-def get_all_domains():
-    return [(domain.id, str(domain.id)) for domain in Domains.query.all()]
 
 
 # def db_warm_up(app, db_name):
