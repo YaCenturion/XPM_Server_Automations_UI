5617893af4194f97391a3240fa6f28547ba1f071
YaCenturion <boris.drozdovsky@gmail.com>
2024-02-28 16:35:42 +0200

    Beta preview: update automation.sh 3

--- automation.sh
+++ automation.sh
@@ -6,6 +6,7 @@
 # expim-ops - name for container
 # 73 - just number tag version for docker when running container
 
+
 ################## GET VARS ######################
 #if [[ $# -eq 3 ]]; then
 folder="${1:-'/srv/SandBox'}"
@@ -23,32 +24,24 @@ if [ "$docker_name" == "expim_NotSetName" ]; then
 fi
 
 
-#else
-#  folder="/srv/SandBox"
-#  docker_name="expim-NotSetName"
-#  tag_version="_no_revision"
-#
-#  echo -e "############ WARNING! ############"
-#  echo -e "FOLDER: $folder and TAG: $tag_version"
-#  echo -e "############# ###### #############"
-#fi
-
 echo -e "############ Docker stop & clear ############"
 docker ps -a
 docker stop "$docker_name"
 docker rm "$docker_name"
 docker container prune -f
 
+
 echo -e "############ Build container $docker_name:prod-v$tag_version ############"
 docker build -t "$docker_name":prod-v"$tag_version" .
 
-echo -e "############ Checking 172.27.0.0/16 docknet ############"
+
+echo -e "############ Checking docknet ############"
 NETWORK_NAME="docknet"
 if docker network inspect $NETWORK_NAME &>/dev/null; then
   echo "Network '$NETWORK_NAME' already exist."
 else
     # Create docker network
-    if docker network create --subnet=172.27.1.0/16 $NETWORK_NAME; then
+    if docker network create --subnet=172.29.3.0/16 $NETWORK_NAME; then
         echo "Network '$NETWORK_NAME' created."
     else
         echo "Error when try create network: '$NETWORK_NAME'."
@@ -56,8 +49,10 @@ else
     fi
 fi
 
+
 echo -e "############ Run container $docker_name:prod-v$tag_version ############"
 docker run -d -e APP_BUILD_VERSION="$tag_version" -v "$folder"/instance:/app/instance -p 80:5000 --network=docknet --name "$docker_name" --restart=unless-stopped "$docker_name":prod-v"$tag_version"
+
 
 echo -e "############ Container started successfully ############"
 exit 0
