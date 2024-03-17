72c63a336773ceffa13ba1739f9fc7017b5dce8d
YaCenturion <boris.drozdovsky@gmail.com>
2024-03-17 09:50:26 +0200

    DEV: add preset users

--- db_presets.py
+++ db_presets.py
@@ -12,18 +12,32 @@ def create_db_and_update_data(app):  # For create DB and fill tables
 
         add_list = [
             # Creating users
-            root_user, user1, user2, user3
+            r_user
         ]
+
+        for row in ui_users:
+            usr = Users(
+                username=row[0],  # type: ignore[call-arg]
+                name=row[1],  # type: ignore[call-arg]
+                email=row[2],  # type: ignore[call-arg]
+                password_hash=generate_password_hash(row[3]),  # type: ignore[call-arg]
+                app_role=row[4],  # type: ignore[call-arg]
+                create=1685782415,  # type: ignore[call-arg]
+                reserved=None  # type: ignore[call-arg]
+            )
+            add_list.append(usr)
+
         add_to_db(add_list)
+
         # Commit
         db.session.commit()
         print('DB created & updated from preset all data\n')
 
 
 # ######################################### Default Users
-root_user = Users(
+r_user = Users(
     username='root',  # type: ignore[call-arg]
-    name='Darth Vader',  # type: ignore[call-arg]
+    name='D. Vader',  # type: ignore[call-arg]
     email='boris.drozdovsky@gmail.com',  # type: ignore[call-arg]
     password_hash=generate_password_hash('xriT8o]xZ7549E2'),  # type: ignore[call-arg]
     app_role=0,  # type: ignore[call-arg]
@@ -31,32 +45,16 @@ root_user = Users(
     reserved=None  # type: ignore[call-arg]
 )
 
-user1 = Users(
-    username='boris',  # type: ignore[call-arg]
-    name='Boris Drozdovski',  # type: ignore[call-arg]
-    email='boris.d@expim.co.il',  # type: ignore[call-arg]
-    password_hash=generate_password_hash('xriT8o]xZ7549E2'),  # type: ignore[call-arg]
-    app_role=1,  # type: ignore[call-arg]
-    create=1685782415,  # type: ignore[call-arg]
-    reserved=None  # type: ignore[call-arg]
-    )
+ui_users = [
+    ('boris', 'Boris Drozdovski', 'boris.d@expim.co.il', 'xriT8o]xZ7549E2', 1),
+    ('liron', 'Liron Hadad', 'liron@expim.co.il', '4P9pVn#uyAscxwwF', 1),
+    ('ofir', 'Ofir Hadad', 'ofir@expim.co.il', 'MasterOfUniverse', 1),
 
-user2 = Users(
-    username='amit.z',  # type: ignore[call-arg]
-    name='Amit Zertal',  # type: ignore[call-arg]
-    email='amit.z@expim.co.il',  # type: ignore[call-arg]
-    password_hash=generate_password_hash('9.DE9v>FdaP=nn'),  # type: ignore[call-arg]
-    app_role=1,  # type: ignore[call-arg]
-    create=1685782415,  # type: ignore[call-arg]
-    reserved=None  # type: ignore[call-arg]
-    )
-
-user3 = Users(
-    username='liron',  # type: ignore[call-arg]
-    name='Liron Hadad',  # type: ignore[call-arg]
-    email='liron@expim.co.il',  # type: ignore[call-arg]
-    password_hash=generate_password_hash('4P9pVn#uyAscxwwF'),  # type: ignore[call-arg]
-    app_role=1,  # type: ignore[call-arg]
-    create=1685782415,  # type: ignore[call-arg]
-    reserved=None  # type: ignore[call-arg]
-    )
+    ('amit.z', 'Amit Zertal', 'amit.z@expim.co.il', '9.DE9v>FdaP=nn', 2),
+    ('david.b', 'David Binaev', 'david.b@expim.co.il', 'RC@X9x1L_4_W>=', 2),
+    ('david.s', 'David Sasson', 'david.s@expim.co.il', 'Ma3q3@d;X+;hbF', 2),
+    ('haim.m', 'Haim Malkukian', 'haim.m@expim.co.il', 'DX12.uB!CwY;m@', 2),
+    ('rafaela.g', 'Rafaela Garnaga', 'rafaela.g@expim.co.il', 'Ra060365!', 2),
+    ('ran.k', 'Ran Kantor', 'ran.k@expim.co.il', 'y6T_1FWmiwbR)g', 2),
+    ('shaked.l', 'Shaked Levi', 'shaked.l@expim.co.il', '5~mE6HV3RcsdtE', 2),
+]
